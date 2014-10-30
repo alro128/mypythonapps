c0c09b634fbae36559fff5e9465fa466bbbf7ef2
alro128 <alvaro128@gmail.com>
2014-10-30 14:35:26 +0100

    format data clean

--- formatData/readData.py
+++ formatData/readData.py
@@ -1,6 +1,6 @@
 # Forma data
 
-basedir = "/Users/alvarovigil/dev/elasticsearch/"
+basedir = "/Users/alro/dev/elasticsearch/"
 i = basedir+"data_books.txt"
 
 c = 0
@@ -16,9 +16,6 @@ for l in f:
   n+=1
   t = "\"title\": \"{0}\", \"author\": \"{1}\", \"cover\": \"{2}\", \"comments\": {3}, \"currency\": \"{4}\", \"pricenew\": {5}, \"priceused\": {6}".format(b[0],b[1],b[2],b[3],b[4],b[5],b[6])
   print "{ " + t.replace('\r', '').replace('\n', '') + "}"
-#  f2 = open(basedir+'books/book_' + str(n), 'w')
-#  f2.write( fila + '\n')
-#  f2.close()
   b = []
 
 f.close()
